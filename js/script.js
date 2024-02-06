e90ae611ebd70f8eab3753ee310403f96b12668e
Quisie Caporte <quisie.caporte@g.msuiit.edu.ph>
2024-02-06 12:11:24 +0800

    review section added

--- js/script.js
+++ js/script.js
@@ -5,3 +5,33 @@ menubtn.onclick = () => {
   menubtn.classList.toggle("fa-times");
   navbar.classList.toggle("active");
 };
+
+// swiper
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
